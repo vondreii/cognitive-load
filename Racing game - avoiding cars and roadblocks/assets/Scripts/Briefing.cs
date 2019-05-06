013a7434730d36ddbafac7d82b25c352a317946a
vondreii <Sharlene.VonDrehnen@outlook.com>
2019-05-07 09:57:42 +1000

    Updated avoiding cars and roadblocks

---
+++ Racing game - avoiding cars and roadblocks/assets/Scripts/Briefing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Briefing : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("In Briefing");
+        
+	}
+}
